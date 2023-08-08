d3fe266d24113de19c8488cc5f229aea1bf93ff7
AnahitaDzl <anahita.dozali@gmail.com>
2023-08-08 20:16:34 +0330

    fix: migration added

--- DebuggingProject/Todo Project/Home/models.py
+++ DebuggingProject/Todo Project/Home/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
-    user = models.ForeignKey(User , on_delete=models.CASCADE)
-    title = models.CharField()
+    title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
 
-    class Meta:
-        abstract = True
+    # class Meta:
+    #     abstract = True
 
     def __str__(self):
         return self.title
